ef7ff2285da743e9c38790e368024f4a896ab855
ahmedjalambo-dev <ahmedjalambo,dev@gmail.com>
2025-07-08 17:38:47 +0300

    Remove Device Preview dependency from pubspec.yaml and update main.dart to run the app directly without Device Preview wrapper.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:newsly/core/cache/shared_prefs_helper.dart';
 import 'package:newsly/core/di/service_locator.dart';
@@ -9,13 +7,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
   await getIt<SharedPrefsHelper>().init();
-  // runApp(const MainApp());
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      backgroundColor: Colors.white,
-      isToolbarVisible: false,
-      builder: (context) => const MainApp(), // Wrap your app
-    ),
-  );
+  runApp(const MainApp());
 }
